7a6faec9351ab0327f3df4043c72ca0b64d69077
Marsahll <iuhnehcuy@gmail.com>
2026-01-13 20:24:41 +0800

    fix(build): 修复 webpack 配置并锁定构建工具版本
    - 简化 vue.config.js 配置结构
    - 添加正确的 webpack alias 配置 (@utils, @mixins 等)
    - 修复 externals 配置格式
    - 锁定 Vue CLI 版本为 4.5.15 (防止自动升级到不兼容版本)
    
    此修复解决了构建时模块解析失败的问题。

--- vue.config.js
+++ vue.config.js
@@ -1,43 +1,23 @@
-const webpack = require('webpack')
 const path = require('path')
-function resolve(dir) {
-  return path.join(__dirname, dir)
-}
 
 module.exports = {
-  transpileDependencies: ['avue-plugin-ueditor'],
-
-  productionSourceMap: false,
-
   configureWebpack: {
-    output: {
-      libraryExport: 'default'
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'packages'),
+        '@components': path.resolve(__dirname, 'packages/components'),
+        '@utils': path.resolve(__dirname, 'packages/utils'),
+        '@mixins': path.resolve(__dirname, 'packages/mixins')
+      }
+    },
+    externals: {
+      vue: {
+        root: 'Vue',
+        commonjs: 'vue',
+        commonjs2: 'vue',
+        amd: 'vue'
+      },
+      'element-ui': 'ELEMENT'
     }
-  },
-
-  chainWebpack: (config) => {
-    config.externals({
-      'vue': 'Vue',
-      'element-ui': 'ElementUI',
-    })
-    config.resolve.alias
-      .set('@', resolve('packages'))
-      .set('@components', resolve('packages/components'))
-      .set('@utils', resolve('packages/utils'))
-      .set('@mixins', resolve('packages/mixins'))
-
-    const output = config.output.store.get('path')
-    if (output.includes('lib')) config.plugin('optimize')
-      .use(webpack.optimize.LimitChunkCountPlugin, [{
-        maxChunks: 1
-      }])
-  },
-
-  devServer: {
-    open: true
-  },
-
-  css: {
-    extract: false
   }
 }
